d3ada80a2cf47ef429ffceb0a38d695a9ee84075
keerthana-golusula <keerthanag9298@gmail.com>
2019-02-06 03:55:24 -0800

    added binary to hexa decimal program

--- bintohex.c
+++ bintohex.c
@@ -2,10 +2,11 @@
 int main(){
 	long int bin;
 	char hex[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-	char res[];
-	int k=1,i=0,rem=0,re=0,j=0,t;
+	char res[20];
+	int k=1,b,i=0,rem=0,re=0,j=0,t;
 	printf("enter binary number");
 	scanf("%ld",&bin);
+	b = bin;
 	while(bin){
 		++i;
 		rem=bin%10;
@@ -14,14 +15,19 @@ int main(){
 		bin=bin/10;
 		if(i%4==0)
 		{
+			//printf("%d",re);
 			res[j++]=hex[re];
+			//printf("%c", res[j]);
 			re=0;
+			k = 1;
 		}
 	}
+//	printf("%d", re);
 	res[j++]=hex[re];
-	printf("%c",res[0]);
-	for(t=sizeof(res)-1;t>=0;t++){
-		printf("%c",res[i]);
+	//printf("%c",res[j]);
+	printf("hexadecimal number of binary number %d is", b);
+	for(t=j-1;t>=0;t--){
+		printf("%c",res[t]);
 	}
 	
 	
